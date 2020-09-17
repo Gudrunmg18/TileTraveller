50face1408346b65877adc65d4563e4899a0fb0b
Baldur Logi <baldurg19@ru.is>
2020-09-17 15:14:19 +0000

    commit

--- verkefni8-TileTraveller.py
+++ verkefni8-TileTraveller.py
@@ -1,2 +1,11 @@
-print('hello')
-print("Hello to you too")
+hnit = [1,1]
+
+direction = input("Direction: ")
+
+if hnit == [3,1]:
+    print("Victory!")
+
+
+print("You can travel: (N)orth.")
+print("Not a valid direction!")
+
